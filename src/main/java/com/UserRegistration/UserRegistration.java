ab935a51a065080fe7c75566bb2fa2224d4c6e5b
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-02 12:53:10 +0530

    ADDED:check first name is valid or not

--- src/main/java/com/UserRegistration/UserRegistration.java
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -1,7 +1,26 @@
 package com.UserRegistration;
-public class UserRegistration {
+import java.util.Scanner;
+public class UserRegistration
+{
     public static void main(String args[])
     {
         System.out.println("--Welcome to user registration --");
+        //name pattern
+        String NAME_PATTERN="^[A-Z][a-z]{2,}$";
+        //create object of scanner
+        Scanner sc=new Scanner(System.in);
+        //take input from user
+        System.out.println("enter string");
+        String str=sc.nextLine();
+        //check name pattern and input name matches
+        if(str.matches(NAME_PATTERN))
+        {
+            System.out.println("valid name");
+        }
+        else
+        {
+            System.out.println("not valid name");
+        }
+
     }
 }
